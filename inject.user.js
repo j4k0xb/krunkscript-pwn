ee2f75bb886e111190d9aad5de593a33c779cc06
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2022-09-21 18:39:18 +0200

    refactor: organise injector

--- inject.user.js
+++ inject.user.js
@@ -14,27 +14,20 @@
 // modify them: krnk.eval('V_someVar = 100')
 // access everything from GAME: krnk.V_PLAYERS.V_list()
 
-const DEFAULT_SCRIPT = `
-"use strict";
-return {render:()=>{},onNetworkMessage:()=>{},onPlayerUpdate:()=>{},start:()=>{},update:()=>{},onPlayerSpawn:()=>{},onPlayerDeath:()=>{},onKeyPress:()=>{},onKeyUp:()=>{},onKeyHeld:()=>{},onMouseClick:()=>{},onMouseUp:()=>{},onMouseScroll:()=>{},onDIVClicked:()=>{}};`;
-
-Object.defineProperty(window, 'krnk', {
-  enumerable: false,
-  configurable: false,
-  writable: false,
-  value: {
-    // inject ur own actions that run additionally
-    render(delta) {
-      const { V_width, V_height } = krnk.V_OVERLAY.V_getSize();
-      for (const player of krnk.V_PLAYERS.V_list()) {
-        if (player.V_isYou || player.V_health <= 0) continue;
-        const pos = krnk.V_SCENE.V_posToScreen(player.V_position.V_x, player.V_position.V_y, player.V_position.V_z);
-        if (!pos.V_onScreen) continue;
-        krnk.V_OVERLAY.V_drawLine(V_width / 2, V_height / 2, pos.V_x, pos.V_y, 2, '#ff0000', 0.8);
-      }
+// inject your own actions that run additionally
+const actions = {
+  render(delta) {
+    const { V_width, V_height } = krnk.V_OVERLAY.V_getSize();
+    for (const player of krnk.V_PLAYERS.V_list()) {
+      if (player.V_isYou || player.V_health <= 0) continue;
+      const pos = krnk.V_SCENE.V_posToScreen(player.V_position.V_x, player.V_position.V_y, player.V_position.V_z);
+      if (!pos.V_onScreen) continue;
+      krnk.V_OVERLAY.V_drawLine(V_width / 2, V_height / 2, pos.V_x, pos.V_y, 2, '#ff0000', 0.8);
     }
-  },
-});
+  }
+};
+
+Object.defineProperty(window, 'krnk', { value: actions });
 
 const originalParse = JSON.parse;
 setTimeout(() => {
@@ -46,11 +39,10 @@ setTimeout(() => {
 }, 2000);
 
 function patchKrunkscript(map) {
-  if (!map.scripts?.client) map.scripts = { client: '' };
-
-  let src = map.scripts.client;
-  if (!src.includes('return')) src = DEFAULT_SCRIPT;
-
+  map.scripts ??= {
+    client: `return{render:()=>{},onNetworkMessage:()=>{},onPlayerUpdate:()=>{},start:()=>{},update:()=>{},onPlayerSpawn:()=>{},onPlayerDeath:()=>{},onKeyPress:()=>{},onKeyUp:()=>{},onKeyHeld:()=>{},onMouseClick:()=>{},onMouseUp:()=>{},onMouseScroll:()=>{},onDIVClicked:()=>{}};`
+  };
+  const src = map.scripts.client;
   const vars = src.match(/^(let.+\n)+/m)?.[0]?.match(/(?<=^let )\w+/gm) || [];
 
   map.scripts.client = `
